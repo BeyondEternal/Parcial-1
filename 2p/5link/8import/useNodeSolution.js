de3db15f55f4796d89a5aa4c632f14f777f47ba5
Mario Solís <jojofan0606@gmail.com>
2022-10-23 03:33:43 -0500

    2do problems

--- 2p/5link/8import/useNodeSolution.js
+++ 2p/5link/8import/useNodeSolution.js
@@ -5,7 +5,6 @@
 import Node from '../../../masc/linked/node.js'
 
 function Linked(n) {//node
-  //this.node = new Node(n)
   this.head = n//node
   this.tail = n//node
   this.size = 1
@@ -14,31 +13,33 @@ function Linked(n) {//node
   this.prepend = prepend
   this.append = append
   this.traverse = traverse
-  this.insertAfter = insertAfter
   this.contains = contains
+  this.getTail = getTail
+  this.insertAfter = insertAfter
+  this.insertBefore = insertBefore
 }
 
-function getHead() { return this.head }
+function getHead(){return this.head}
 
-function prepend(n) {//new node
+function prepend(n){//new node
   n.next = this.head
-  this.head = n
+  this.head=n
   this.size++
 }
 
-function append(n) {//new node
+function append(n){//new node
   n.next = null//
-  this.tail.next = n
-  this.tail = n
+  this.tail.next=n
+  this.tail=n
   this.size++
 }
 
-function traverse() {
-  let c = this.head
-  while (c) {//exists
-    //console.log(c.key)
-    console.log(c.data)
-    c = c.next
+function traverse(){
+  let c=this.head
+  while(c){//exists
+//console.log(c.key)
+console.log(c.data)
+c=c.next
   }
   console.log('\n')
 }
@@ -46,23 +47,60 @@ function traverse() {
 //
 // exercises
 //
-function contains(v) {
-  let currNode = this.head
-  while (currNode != item) {
-    currNode = currNode.next
+function contains(v){
+  let c = this.head
+  while(c){
+      if(v==c.data){
+          return true
+      }        
+      c=c.next
   }
-  return currNode
+  return false
+}
+function getTail(){
+  return this.tail.data    
+}
+function insertAfter(position,newNode){
+  let c=this.head
+  let currNode
+  while(c){
+    if(position===c){
+      currNode = c.next
+      c.next = newNode
+      newNode.next = currNode
+
+    }
+    c = c.next
+  }
+}
+function insertBefore(position,newNode){
+  let c=this.head
+  let prevNode,currNode
+  while(c){
+    if(position === this.head){
+      this.prepend(newNode)
+    }
+    else if(position===c){
+      currNode = prevNode.next
+      prevNode.next = newNode
+      newNode.next = currNode
+    }
+    else{
+      prevNode = c
+    }
+    c = c.next
+  }
 }
 
-function insertAfter(newNode, element) {
-  let currNode = contains(element)
-  if(contains(element)===element){
-    currNode.next = newNode
-  }
-}
 
 
-let n = new Linked('b')
-n.insertAfter('a', 'b')
 
-console.log(n)
+let n = new Node('a')
+let n1 = new Node('b')
+let n2 = new Node('d')
+let n3 = new Node('c')
+let  l = new Linked(n)
+l.append(n1)
+l.append(n2)
+l.insertAfter(n2,n3)
+l.traverse()
