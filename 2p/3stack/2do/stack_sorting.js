7cd758378932f57de5b81c57c6b6f304338dfb3a
Mario Solís <jojofan0606@gmail.com>
2022-10-23 23:17:11 -0500

    Finishing practice stack&queue

--- 2p/3stack/2do/stack_sorting.js
+++ 2p/3stack/2do/stack_sorting.js
@@ -1,86 +1,112 @@
-function pila(maximo) {
-    this.Tamaño_Pila = []
-    this.max = maximo
-    this.llena = llena
-    this.vacia = vacia
-    this.entrada = entrada
-    this.salida = salida
-    this.mostrar = mostrar
-    this.peek = peek
-  }
-  
-  function entrada(element) {
-    if (this.llena()) {
-      console.log("Tu pila esta llena, debes eliminar elementos")
-    } else {
-      this.Tamaño_Pila.unshift(element)
+let x = [10, 100, 1000]
+
+for(let i = 0;i<3;i++){
+  function pila(maximo) {
+      this.Tamaño_Pila = []
+      this.max = maximo
+      this.llena = llena
+      this.vacia = vacia
+      this.entrada = entrada
+      this.salida = salida
+      this.mostrar = mostrar
+      this.peek = peek
     }
-  }
-  
-  function salida() {
-    if (this.vacia()) {
-      console.log("Tu pila esta vacia, debes introducir elementos")
+    
+    function entrada(element) {
+      if (this.llena()) {
+        console.log("Tu pila esta llena, debes eliminar elementos")
+      } else {
+        this.Tamaño_Pila.unshift(element)
+      }
     }
-    else {
-      return this.Tamaño_Pila.shift()
+    
+    function salida() {
+      if (this.vacia()) {
+        console.log("Tu pila esta vacia, debes introducir elementos")
+      }
+      else {
+        return this.Tamaño_Pila.shift()
+      }
     }
-  }
-  
-  function llena() {
-    if (this.Tamaño_Pila.length === this.max)
-      return true
-    else
-      return false
-  }
-  
-  function vacia() {
-    if (this.Tamaño_Pila.length === 0)
-      return true
-    else
-      return false
-  }
-  
-  function mostrar() {
-    let show = ""
-    for (let i = 0; i < this.Tamaño_Pila.length; ++i) {
-      show += this.Tamaño_Pila[i] + "\n"
+    
+    function llena() {
+      if (this.Tamaño_Pila.length === this.max)
+        return true
+      else
+        return false
     }
-    return show
-  }
-  
-  function peek() {
-    return this.Tamaño_Pila[0]
-  }
-  let k=0
-  let t1=performance.now()
-  function ord_pila(x) {
-    let p = new pila(x);
-    let s = new pila(x);
-    let n, r;
-    let i = 1;
-    n = Math.floor(Math.random() * (x - 1) + 1)
-    p.entrada(n)
-    while (i < x) {
-        while(!s.vacia()){
-            p.entrada(s.salida())
-          k++
-        }
-      r = Math.floor(Math.random() * (x - 1) + 1)    
-      while(r>=p.peek() && !p.vacia()){
-          s.entrada(p.salida())
-        k++
-        }  
-        if(r<p.peek() || p.vacia()){
-            p.entrada(r)          
+    
+    function vacia() {
+      if (this.Tamaño_Pila.length === 0)
+        return true
+      else
+        return false
+    }
+    
+    function mostrar() {
+      let show = ""
+      for (let i = 0; i < this.Tamaño_Pila.length; ++i) {
+        show += this.Tamaño_Pila[i] + "\n"
+      }
+      return show
+    }
+    
+    function peek() {
+      return this.Tamaño_Pila[0]
+    }
+    let k=0,t1,t2
+    function ord_pila(x) {
+      k=0
+      t1=performance.now()
+      let p = new pila(x);
+      let s = new pila(x);
+      let n, r;
+      let i = 1;
+      n = Math.floor(Math.random() * (x - 1) + 1)
+      p.entrada(n)
+      while (i < x) {
+          while(!s.vacia()){
+              p.entrada(s.salida())
+            k++
           }
-        i++
+        r = Math.floor(Math.random() * (x - 1) + 1)    
+        while(r>=p.peek() && !p.vacia()){
+            s.entrada(p.salida())
+          k++
+          }  
+          if(r<p.peek() || p.vacia()){
+              p.entrada(r)          
+            }
+          i++
+      }
+      while(!s.vacia()){
+          p.entrada(s.salida())
+      }
+      console.log(p.mostrar())
+      console.log(s.mostrar())
+      t2=performance.now()
     }
-    while(!s.vacia()){
-        p.entrada(s.salida())
+
+    let kArray = [], tArray = []
+
+    for(let j = 0;j<5;j++){
+      ord_pila(x[i])
+      tArray[j]=t2-t1
+      kArray[j]=k
     }
-    console.log(p.mostrar())
-    console.log(s.mostrar())
+    console.log("Tiempo de ejecución: ", tArray)
+    console.log("Pasos: ", kArray)
+    console.log("Promedio de tiempo: ", average(tArray))
+    console.log("Promedio de pasos: ",average(kArray));
+  
+}
+
+
+function average(array) {
+  let average=0
+  for(let i = 0;i<array.length-1;i++){
+    average+=array[i]
   }
-  let t2=performance.now()
-  ord_pila(1000)
-  console.log("Tiempo de ejecución: ",t2-t1)
+  average/=(array.length-1)
+  return average
+}
--- 2p/4queue/2do/queue_sorting.js
+++ 2p/4queue/2do/queue_sorting.js
@@ -1,111 +1,133 @@
-function fila(maximo) {
-  this.Tamaño_Fila = []
-  this.max = maximo
-  this.llena = llena
-  this.vacia = vacia
-  this.entrada = entrada
-  this.entradaSort = entradaSort
-  this.salida = salida
-  this.salidaSort = salidaSort
-  this.mostrar = mostrar
-  this.peek = peek
-}
-//add
-function entrada(element) {
-  if (this.llena()) {
-    console.log("Tu pila esta llena, debes eliminar elementos")
-  } else {
-    this.Tamaño_Fila.push(element)
+let x = [10, 100, 1000]
+for(let i = 0;i<3;i++){
+  function fila(maximo) {
+    this.Tamaño_Fila = []
+    this.max = maximo
+    this.llena = llena
+    this.vacia = vacia
+    this.entrada = entrada
+    this.entradaSort = entradaSort
+    this.salida = salida
+    this.salidaSort = salidaSort
+    this.mostrar = mostrar
+    this.peek = peek
   }
-}
-
-function salida() {
-  if (this.vacia()) {
-    console.log("Tu pila esta vacia, debes introducir elementos")
-  }
-  else {
-    return this.Tamaño_Fila.shift()
-  }
-}
-function entradaSort(element) {
-  if (this.llena()) {
-    console.log("Tu pila esta llena, debes eliminar elementos")
-  } else {
-    this.Tamaño_Fila.push(element)
-  }
-}
-
-function salidaSort() {
-  if (this.vacia()) {
-    console.log("Tu pila esta vacia, debes introducir elementos")
-  }
-  else {
-    return this.Tamaño_Fila.shift()
-  }
-}
-
-function llena() {
-  if (this.Tamaño_Fila.length === this.max)
-    return true
-  else
-    return false
-}
-
-function vacia() {
-  if (this.Tamaño_Fila.length === 0)
-    return true
-  else
-    return false
-}
-
-function mostrar() {
-  let show = ""
-  for (let i = 0; i < this.Tamaño_Fila.length; ++i) {
-    show += this.Tamaño_Fila[i] + "\n"
-  }
-  return show
-}
-
-function peek() {
-  return this.Tamaño_Fila[0]
-}
-let k = 0;
-let t1 = performance.now()
-function ord_fila(x) {
-  let p = new fila(x);
-  let s = new fila(x);
-  let n, r;
-  let i = 1;
-  n = Math.floor(Math.random() * (x - 1) + 1)
-  p.entrada(n)
-  while (i < x) {
-    r = Math.floor(Math.random() * (x - 1) + 1)
-    while (!p.vacia()) {
-      s.entrada(p.salida())
-      k++
+  //add
+  function entrada(element) {
+    if (this.llena()) {
+      console.log("Tu pila esta llena, debes eliminar elementos")
+    } else {
+      this.Tamaño_Fila.push(element)
     }
-    while (r > s.peek()) {
-      p.entrada(s.salida())
-      k++
-    }
-    if (s.vacia()) {
-      p.entrada(r)
-    }
-    if (r <= s.peek()) {
-      p.entrada(r)
-
-    }
-    while (!s.vacia()) {
-      p.entrada(s.salida())
-      k++
-    }
-    i++
   }
-  console.log(p.mostrar())
-  console.log(s.mostrar())
-}
-let t2 = performance.now()
+  
+  function salida() {
+    if (this.vacia()) {
+      console.log("Tu pila esta vacia, debes introducir elementos")
+    }
+    else {
+      return this.Tamaño_Fila.shift()
+    }
+  }
+  function entradaSort(element) {
+    if (this.llena()) {
+      console.log("Tu pila esta llena, debes eliminar elementos")
+    } else {
+      this.Tamaño_Fila.push(element)
+    }
+  }
+  
+  function salidaSort() {
+    if (this.vacia()) {
+      console.log("Tu pila esta vacia, debes introducir elementos")
+    }
+    else {
+      return this.Tamaño_Fila.shift()
+    }
+  }
+  
+  function llena() {
+    if (this.Tamaño_Fila.length === this.max)
+      return true
+    else
+      return false
+  }
+  
+  function vacia() {
+    if (this.Tamaño_Fila.length === 0)
+      return true
+    else
+      return false
+  }
+  
+  function mostrar() {
+    let show = ""
+    for (let i = 0; i < this.Tamaño_Fila.length; ++i) {
+      show += this.Tamaño_Fila[i] + "\n"
+    }
+    return show
+  }
+  
+  function peek() {
+    return this.Tamaño_Fila[0]
+  }
+  let k = 0;
+  let t2
+  let t1
+  function ord_fila(x) {
+    t1 = performance.now()
+    k=0
+    let p = new fila(x);
+    let s = new fila(x);
+    let n, r;
+    let i = 1;
+    n = Math.floor(Math.random() * (x - 1) + 1)
+    p.entrada(n)
+    while (i < x) {
+      r = Math.floor(Math.random() * (x - 1) + 1)
+      while (!p.vacia()) {
+        s.entrada(p.salida())
+        k++
+      }
+      while (r > s.peek()) {
+        p.entrada(s.salida())
+        k++
+      }
+      if (s.vacia()) {
+        p.entrada(r)
+      }
+      if (r <= s.peek()) {
+        p.entrada(r)
+  
+      }
+      while (!s.vacia()) {
+        p.entrada(s.salida())
+        k++
+      }
+      i++
+    }
+    console.log(p.mostrar())
+    console.log(s.mostrar())
+    t2 = performance.now()
+  }
+  let kArray = [], tArray = []
 
-ord_fila(1000)
-console.log("Tiempo de ejecución: ", t2 - t1)
-console.log("Pasos: ", k)
+  for(let j = 0;j<5;j++){
+    ord_fila(x[i])
+    tArray[j]=t2-t1
+    kArray[j]=k
+  }
+  console.log("Tiempo de ejecución: ", tArray)
+  console.log("Pasos: ", kArray)
+  console.log("Promedio de tiempo: ", average(tArray))
+  console.log("Promedio de pasos: ",average(kArray));
+}
+
+function average(array) {
+  let average=0
+  for(let i = 0;i<array.length-1;i++){
+    average+=array[i]
+  }
+  average/=(array.length-1)
+  return average
+}
