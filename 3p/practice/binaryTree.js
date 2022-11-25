12bb581549ff37419e711458c80ee2205b025878
Mario Solís <jojofan0606@gmail.com>
2022-11-24 19:09:26 -0600

    binaryTree updated

--- 3p/practice/binaryTree.js
+++ 3p/practice/binaryTree.js
@@ -1,140 +1,86 @@
-class Node{
-   constructor(data)
-   {
-       this.data = data;
-       this.left = null;
-       this.right = null;
-   }
-}
-
-export default class BinaryTree{
-   constructor(){
-       this.root=null
-   }
-   
-
-   insertNode(node,newNode){
-            if(this.root==null){
-               return this.root=newNode
-            }
-           if(node.left==null){
-               return node.left=newNode
-           }
-           else if(node.right==null){
-               return node.right=newNode
-           }
-           else{
-               return false
-           }
-       }
-
-       deleteDeepest(root, delNode)
-    {
-        let q = [];
-        q.push(root);
- 
-        let temp = null;
- 
-        // Do level order traversal until last node
-        while (q.length > 0)
-        {
-            temp = q[0];
-            q.shift();
- 
-            if (temp == delNode)
-            {
-                temp = null;
-                return;
- 
-            }
-            if (temp.right!=null)
-            {
-                if (temp.right == delNode)
-                {
-                    temp.right = null;
-                    return;
-            }
-            else
-                q.push(temp.right);
-        }
- 
-        if (temp.left != null)
-        {
-            if (temp.left == delNode)
-            {
-                temp.left = null;
-                return;
-            }
-            else
-                q.push(temp.left);
-        }
+class BinaryTreeNode {
+    constructor(key, value = key, parent = null) {
+      this.key = key;
+      this.value = value;
+      this.parent = parent;
+      this.left = null;
+      this.right = null;
     }
+  
+    get isLeaf() {
+      return this.left === null && this.right === null;
     }
-
-       delete(root, key)
-       {
-           if (root == null)
-               return;
-    
-           if (root.left == null &&
-              root.right == null)
-           {
-               if (root.key == key)
-               {
-                     root=null;
-                     return;
-               }
-               else
-                   return;
-           }
-    
-           let q = [];
-           q.push(root);
-           let temp = null, keyNode = null;
-    
-           // Do level order traversal until
-           // we find key and last node.
-           while (q.length > 0)
-           {
-               temp = q[0];
-               q.shift();
-    
-               if (temp.key == key)
-                   keyNode = temp;
-    
-               if (temp.left != null)
-                   q.push(temp.left);
-    
-               if (temp.right != null)
-                   q.push(temp.right);
-           }
-    
-           if (keyNode != null)
-           {
-               let x = temp.key;
-               deleteDeepest(root, temp);
-               keyNode.key = x;
-           }
-       }
-
-   search(data){
-       this.searchNode(this.root,data)
-   }
-
-   searchNode(node,data){
-       if(node==null){
-           return null
-       }
-       else if(data<node.data){
-           return this.searchNode(node.left,data)
-       }
-       else if(data>node.data){
-           return this.searchNode(node.right,data)
-       }
-       else{
-           return node
-       }
-   }
-}
-
-
+  
+    get hasChildren() {
+      return !this.isLeaf;
+    }
+  }
+  
+  export default class BinaryTree {
+    constructor(key, value = key) {
+      this.root = new BinaryTreeNode(key, value);
+    }
+  
+    *inOrderTraversal(node = this.root) {
+      if (node.left) yield* this.inOrderTraversal(node.left);
+      yield node;
+      if (node.right) yield* this.inOrderTraversal(node.right);
+    }
+  
+    *postOrderTraversal(node = this.root) {
+      if (node.left) yield* this.postOrderTraversal(node.left);
+      if (node.right) yield* this.postOrderTraversal(node.right);
+      yield node;
+    }
+  
+    *preOrderTraversal(node = this.root) {
+      yield node;
+      if (node.left) yield* this.preOrderTraversal(node.left);
+      if (node.right) yield* this.preOrderTraversal(node.right);
+    }
+  
+    insert(
+      parentNodeKey,
+      key,
+      value = key,
+      { left, right } = { left: true, right: true }
+    ) {
+      for (let node of this.preOrderTraversal()) {
+        if (node.key === parentNodeKey) {
+          const canInsertLeft = left && node.left === null;
+          const canInsertRight = right && node.right === null;
+          if (!canInsertLeft && !canInsertRight) return false;
+          if (canInsertLeft) {
+            node.left = new BinaryTreeNode(key, value, node);
+            return true;
+          }
+          if (canInsertRight) {
+            node.right = new BinaryTreeNode(key, value, node);
+            return true;
+          }
+        }
+      }
+      return false;
+    }
+  
+    remove(key) {
+      for (let node of this.preOrderTraversal()) {
+        if (node.left.key === key) {
+          node.left = null;
+          return true;
+        }
+        if (node.right.key === key) {
+          node.right = null;
+          return true;
+        }
+      }
+      return false;
+    }
+  
+    find(key) {
+      for (let node of this.preOrderTraversal()) {
+        if (node.key === key) return node;
+      }
+      return undefined;
+    }
+  }
--- 3p/practice/useBinaryTree.js
+++ 3p/practice/useBinaryTree.js
@@ -1,10 +1,27 @@
-import binaryTree from "./binaryTree.js";
+import BinaryTree from "./binaryTree.js";
 
-let p = new binaryTree('+')
-let o1 = new binaryTree(3)
-let o2 = new binaryTree(-1)
+const tree = new BinaryTree(1, 'AB');
 
-p.left = o1
-p.right = o2
+tree.insert(1, 11, 'AC');
+tree.insert(1, 12, 'BC');
+tree.insert(12, 121, 'BG', { right: true });
 
-console.log(p);
+console.log([...tree.preOrderTraversal()].map(x => x.value))
+
+
+console.log([...tree.inOrderTraversal()].map(x => x.value))
+
+
+tree.root.value;                
+tree.root.hasChildren;          
+
+tree.find(12).isLeaf;           
+tree.find(121).isLeaf;          
+tree.find(121).parent.value;    
+tree.find(12).left;             
+tree.find(12).right.value;      
+
+tree.remove(12);
+
+[...tree.postOrderTraversal()].map(x => x.value);
+
